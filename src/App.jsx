41131844b744722abfee51fdd343b262fce8c69f
Govind365 <govindndp7@gmail.com>
2024-08-23 12:30:59 +0530

    this is added

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,8 @@ import React from 'react'
 const App = () => {
   return (
        <div>
-        <h1>I am from Global-g branch</h1>
-        <div>Hey ! Welcome to global fpo </div>
-        <h2>I am from pulldata branch</h2>
-        <div>welcome to global fpo development team</div>
+        <h3>Learning programming is not a cup of tea it will take lot of time practice ,petience and hard work</h3>
+        <h3>You have to practice every day with the real time problem</h3>
        </div>
   )
 }
