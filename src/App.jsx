926f679a31024232161c285509a086a47ab00f9c
Govind365 <govindndp7@gmail.com>
2024-08-23 11:36:57 +0530

    with heading

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
        <div>
+        <h1>I am from Global-g branch</h1>
         <div>Hey ! Welcome to global fpo </div>
+        <h2>I am from pulldata branch</h2>
         <div>welcome to global fpo development team</div>
        </div>
   )
